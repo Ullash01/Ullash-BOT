5a4121448e3b4e6697a592089091237c8b372cf8
Ullash01 <ullash264@gmail.com>
2024-10-21 08:24:33 +0600

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -12,7 +12,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
-  const permission = [ "100029901980367","100029901980367"];
+  const permission = [ "100086680386976","100086680386976"];
    if (!permission.includes(event.senderID))
    return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
